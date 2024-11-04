88336b8535fd203405f46d07f4d60d3f481bbfde
hoangnamlee04 <150770315+hoangnamlee04@users.noreply.github.com>
2024-11-04 11:57:53 +0700

    sửa app.tsx

--- Charity/client/src/App.tsx
+++ Charity/client/src/App.tsx
@@ -5,11 +5,11 @@ import About from "./pages/website/About/About";
 import Contact from "./pages/website/Contact/Contact";
 import Causes from "./pages/website/Causes/Causes";
 import CauseDetails from "./pages/website/Cause-details/Cause-details";
-import Volunteer from "./pages/website/volunteer/Volunteer";
-import Site from "./pages/website/Site/Site";
-import Blog from "./pages/website/Blog/Blog";
-import BlogDetails from "./pages/website/Blog-details/Blog-details";
-import VolunteerDetails from "./pages/website/Volunteer-details/volunteer-details";
+import Events from "./pages/website/Events/Events";
+import EventsDetails from "./pages/website/Events-details/Events-details";
+import Donation from "./pages/website/Donation/Donation";
+import Mission from "./pages/website/Mission/Mission";
+import Faq from "./pages/website/Faq/Faq";
 
 function App() {
   return (
@@ -19,11 +19,11 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/causes" element={<Causes />} />
         <Route path="/cause-details" element={<CauseDetails />} />
-        <Route path="/volunteer" element={<Volunteer />} />
-        <Route path="/volunteer-details" element={<VolunteerDetails />} />
-        <Route path="/site" element={<Site />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blog-details" element={<BlogDetails />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/events-details" element={<EventsDetails />} />
+        <Route path="/donation" element={<Donation />} />
+        <Route path="/mission" element={<Mission />} />
+        <Route path="/faq" element={<Faq />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
