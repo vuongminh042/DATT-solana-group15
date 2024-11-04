3ff0f2619faec0fd5f3127bdeca3f337c1231e99
Quynh12345678 <phunhmanhquynh@gmail.com>
2024-11-04 11:27:20 +0700

    gd/login/register

--- Charity/src/App.tsx
+++ Charity/src/App.tsx
@@ -1,13 +1,17 @@
-
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./component/Login";
+import Register from "./component/Register";
 
 function App() {
-
   return (
     <>
-
+      <Routes>
+        <Route path="/login" Component={Login} />
+        <Route path="/register" Component={Register} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
