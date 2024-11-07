9f46fcc435f9c7b5be43cedf1b3155ef870c2d51
TranMinhDuc2003 <106735778+TranMinhDuc2003@users.noreply.github.com>
2024-11-07 16:30:02 +0700

    check role

--- Charity/server/src/router/category.js
+++ Charity/server/src/router/category.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import CategoryController from "../controller/CategoryController.js";
+import { checkAuth, checkRoles } from "../middlewear/checkAuth.js";
 
 const routerCategory = Router()
 
-routerCategory.post('/', CategoryController.create)
 routerCategory.get('/', CategoryController.get)
 routerCategory.get('/:id', CategoryController.getDetail)
+
+routerCategory.use('/', checkAuth, checkRoles(['admin']))
+routerCategory.post('/', CategoryController.create)
 routerCategory.delete('/:id', CategoryController.delete)
 routerCategory.patch('/:id', CategoryController.update)
 
--- Charity/server/src/router/contact.js
+++ Charity/server/src/router/contact.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import ContactController from "../controller/ContactController.js";
+import { checkAuth, checkRoles } from "../middlewear/checkAuth.js";
 const routerContact = Router()
 
-routerContact.post('/', ContactController.create)
 routerContact.get('/', ContactController.get)
 routerContact.get('/:id', ContactController.getDetail)
+
+routerContact.use('/', checkAuth, checkRoles(['admin']))
+routerContact.post('/', ContactController.create)
 routerContact.delete('/:id', ContactController.delete)
 routerContact.patch('/:id', ContactController.update)
 
